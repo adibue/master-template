28d02ca5c2bc46673946eeb5a8f90e1028b33455
Adrian Bühler <48823479+adibue@users.noreply.github.com>
2026-07-08 10:22:11 +0200

    Add metadata.sh example file

---
+++ Code/metadata.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Metadata:
+# Target Platform: unspecified
+# Maintained by: adibue
+# Source: https://github.com/adibue/master-template
+# Desc: Metadata example for using in scripts and other code files.
+# Depends on:
+#		- no dependencies
